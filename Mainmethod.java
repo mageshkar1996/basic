45cf01fa9939b36c2b2a8b2eea06471c3a423946
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-07 20:55:39 +0530

    Add files via upload
    what happens if there are multiple main methods inside one class in java?
    
    Mainmethod.java:7: error: method main(String[]) is already defined in class Mainmethod
    public static void main(String[] args)
                       ^
    1 error
    
    you will error like this

---
+++ Mainmethod.java
@@ -0,0 +1,12 @@
+public class Mainmethod
+{
+public static void main(String[] args)
+{
+System.out.println("welcome java");
+}
+public static void main(String[] args)
+{
+System.out.println("welcome java1");
+}
+
+}
