31021dbfdb09924d41c5a9e966b4b526730ad7f6
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-07 15:40:05 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,19 @@
+package jan.mag;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Input
+{
+public static void main(String[] args)
+{
+System.out.println("welcome to java");
+Scanner obj = new Scanner(System.in);
+String name=obj.nextLine();
+System.out.println("our first input:"+name);
+int num1=obj.nextInt();
+System.out.println("our first number:"+num1);
+int num2=obj.nextInt();
+System.out.println("our first number:"+num2);
+system.out.println(boolean(num1>num2));
+}
+}
