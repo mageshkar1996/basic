a8dffc2cc16d297326c97d362ca8045e025cbf91
Mageshkar <96710996+mageshkar1996@users.noreply.github.com>
2022-01-11 22:50:01 +0530

    Add files via upload
    overloading :
    1.largest integer number in two input.
    2.largest double number in two input
    3. largest char in two input
    4.largest integer number in three input.
    5. largest char in three input

--- OverLoading.java
+++ OverLoading.java
@@ -7,6 +7,8 @@ public static void main(String[] args)
         System.out.println(obj.max(4,5));
         System.out.println(obj.max(4.7,5.8));
         System.out.println(obj.max('a','b'));
+        System.out.println(obj.max(14,18,98));
+        System.out.println(obj.max('f','e','t'));
 }
 public int max(int a,int b)
 {
@@ -19,6 +21,14 @@ return a>b?a:b;
 public char max(char a,char b)
 {
 return a>b?a:b;    
+}
+public int max(int a,int b,int c)
+{
+return (a>b)?(a>c?a:c):(b>c?b:c);
+}
+public char max(char a,char b,char c)
+{
+return (a>b)?(a>c?a:c):(b>c?b:c);    
 }
 
 
